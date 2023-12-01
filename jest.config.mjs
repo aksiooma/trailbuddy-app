24f6a0c4184e9ec3db26be52743abc23a3ebf9aa
aksiooma <teijo.virta@gmail.com>
2023-12-01 13:51:50 +0200

    setting the test enviroment. Added cross-env package.

--- jest.config.mjs
+++ jest.config.mjs
@@ -9,12 +9,12 @@ const customJestConfig = {
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   testEnvironment: 'jest-environment-jsdom',
   testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
-  // If your test files are named as *.test.tsx or *.test.ts:
+  // If test files are named as *.test.tsx or *.test.ts:
   testMatch: [
     '**/__tests__/**/*.tsx', // Looks for any files inside __tests__ folders with a .tsx extension
     '**/?(*.)+(spec|test).tsx', // Looks for any files ending in .test.tsx or .spec.tsx
   ],
-  // If you're using TypeScript, you may need to add a transform property as well
+  // If using TypeScript, may need to add a transform property
   transform: {
     '^.+\\.(ts|tsx)$': 'babel-jest',
   },
